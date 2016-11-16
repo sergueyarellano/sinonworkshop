ef16025f6610d2d8db3822f60dda3625b2e276c0
Serguey Arellano <serguey.arellano.contractor@bbva.com>
2016-11-17 00:56:04 +0100

    add blue-tape dep && style

--- test/lib/database.test.js
+++ test/lib/database.test.js
@@ -1,50 +1,49 @@
-const test = require('tape'),
-    sinon = require('sinon'),
-    databaseConnection = require('../../lib/database.js')
-
+const test = require('tape')
+const sinon = require('sinon')
+const databaseConnection = require('../../lib/database.js')
 
 test('get Full name from database', (assert) => {
-    const response = {
-            userName: 'David',
-            firstName: 'Rodríguez',
-            nickName: 'xrace300'
-        },
-        stub = sinon.stub(databaseConnection, 'getUserInfo').returns(response)
+  const response = {
+    userName: 'David',
+    firstName: 'Rodríguez',
+    nickName: 'xrace300'
+  }
+  const stub = sinon.stub(databaseConnection, 'getUserInfo').returns(response)
 
-    assert.deepEqual(databaseConnection.getUserInfo(), response)
-    stub.restore()
-    assert.end()
+  assert.deepEqual(databaseConnection.getUserInfo(), response)
+  stub.restore()
+  assert.end()
 })
 
 test('get User info FOR DAVID ONLY', (assert) => {
-    const response = {
-        userName: 'David',
-        firstName: 'Rodríguez',
-        nickName: 'xrace300'
-    }
+  const response = {
+    userName: 'David',
+    firstName: 'Rodríguez',
+    nickName: 'xrace300'
+  }
 
-    let mock = sinon.mock(databaseConnection)
+  let mock = sinon.mock(databaseConnection)
 
-    mock.expects('getUserInfo').returns(response)
+  mock.expects('getUserInfo').returns(response)
 
-    assert.deepEqual(databaseConnection.getUserInfo('David'), response)
-    assert.end()
-    mock.verify()
+  assert.deepEqual(databaseConnection.getUserInfo('David'), response)
+  assert.end()
+  mock.verify()
 })
 
 test('get Error FOR INVALID USER', (assert) => {
-    const mock = sinon.mock(databaseConnection),
-        spy = sinon.spy(databaseConnection, 'getManOfTheDay')
+  const mock = sinon.mock(databaseConnection)
+  const spy = sinon.spy(databaseConnection, 'getManOfTheDay')
 
-    mock.expects('getUserInfo').once().withArgs('Serguey').throws()
+  mock.expects('getUserInfo').once().withArgs('Serguey').throws()
 
-    try {
-        console.log(databaseConnection.getManOfTheDay())
-    } catch (error) {
-        console.log('Error capturado!!!')
-    }
+  try {
+    console.log(databaseConnection.getManOfTheDay())
+  } catch (error) {
+    console.log('Error capturado!!!')
+  }
 
-    assert.true(spy.threw());
-    assert.end()
-    mock.verify()
+  assert.true(spy.threw())
+  assert.end()
+  mock.verify()
 })
--- test/lib/promises.test.js
+++ test/lib/promises.test.js
@@ -1,25 +1,23 @@
-const test = require('tape')
+const test = require('blue-tape')
 const promises = require('../../lib/promises.js')
 
 test('toss: should toss a 6', (assert) => {
   const expected = 6
 
-  promises.toss(6)
+  return promises.toss(6)
   .then(actual => {
     assert.deepEqual(actual, expected)
   })
-  assert.end()
 })
 
 test('toss: should toss an error', (assert) => {
   const expected = 'Not a six'
 
-  promises.toss(4)
+  return promises.toss(4)
   .then(actual => {
     // rejects value, throw error
   })
   .catch(error => {
     assert.deepEqual(error, expected)
   })
-  assert.end()
 })
