a36fa096b4545b2cd44819db8b5fed3605849dc0
Serguey Arellano <serguey.arellano.contractor@bbva.com>
2016-12-01 12:05:45 +0100

    add modifications from the last week

--- test/lib/marvel.test.js
+++ test/lib/marvel.test.js
@@ -1,10 +1,9 @@
 require('../env/initBackground')();
-
 const render = require('../../lib/render');
 const handler = require('../../lib/handler');
 
-test('renderFantasticButton: should render button', (assert) => {
-  render.renderFantasticButton('click me!');
+test('renderButton: should render button', (assert) => {
+  render.renderButton('click me!');
   let actual = $('button').text();
   let expected = 'click me!';
 
@@ -13,59 +12,84 @@ test('renderFantasticButton: should render button', (assert) => {
   assert.end();
 });
 
-test('renderFantasticButton: button sould hide on click', (assert) => {
-  let stub = sinon.stub(handler, 'callMarvel');
-  render.renderFantasticButton('click me again!');
+test('renderButton: should hide button on click', (assert) => {
+  render.renderButton();
   $('button').click();
-  let actual = $('button').css('display');
-  let expected = 'none';
+  let actual = $('button').is(':hidden');
 
-  assert.deepEqual(actual, expected);
-
-  $('button').remove();
-  stub.restore();
+  assert.ok(actual);
   assert.end();
 });
 
-test('renderImage: should render a image', (assert) => {
-  let path = 'my/path';
-  let extension = 'jpg';
-  let name = 'Spider-Man';
-  let desc = 'Bitten by a spider';
-  render.renderImage(path, extension, name, desc);
-  let actual = $('img').attr('src');
-  let expected = 'my/path.jpg';
-  assert.deepEqual(actual, expected);
+handler.callMarvel('spider-man');
 
-  // do not forget to leave the environment and DOM clean
-  $('img[src="my/path.jpg"]').parent().remove();
-  assert.end();
-});
-
-test('INTEGRATION: clicking on button should display a profile of spider man', (assert) => {
-  let path = 'my/path';
-  let ext = 'jpg';
-  let name = 'Spider-Man';
-  let desc = 'Bitten by a spider';
-  let stub = sinon.stub(handler, 'callMarvel');
-
-  render.renderFantasticButton('click me!');
-  $('button').click();
-  stub.returns(render.renderImage(path, ext, name, desc));
-  let actual = $('img').css('display');
-  let expected = 'block';
-
-  assert.deepEqual(actual, expected);
-
-  $('button').remove();
-  $('img').parent().remove();
-  stub.restore();
-  assert.end();
-});
-
-// remove
-test('for presentation purposes', (assert) => {
-  render.renderFantasticButton('click me!');
-  assert.ok(true);
-  assert.end();
-});
+//
+// const render = require('../../lib/render');
+// const handler = require('../../lib/handler');
+//
+// test('renderFantasticButton: should render button', (assert) => {
+//   render.renderFantasticButton('click me!');
+//   let actual = $('button').text();
+//   let expected = 'click me!';
+//
+//   assert.deepEqual(actual, expected);
+//   $('button').remove();
+//   assert.end();
+// });
+//
+// test('renderFantasticButton: button sould hide on click', (assert) => {
+//   let stub = sinon.stub(handler, 'callMarvel');
+//   render.renderFantasticButton('click me again!');
+//   $('button').click();
+//   let actual = $('button').css('display');
+//   let expected = 'none';
+//
+//   assert.deepEqual(actual, expected);
+//
+//   $('button').remove();
+//   stub.restore();
+//   assert.end();
+// });
+//
+// test('renderImage: should render a image', (assert) => {
+//   let path = 'my/path';
+//   let extension = 'jpg';
+//   let name = 'Spider-Man';
+//   let desc = 'Bitten by a spider';
+//   render.renderImage(path, extension, name, desc);
+//   let actual = $('img').attr('src');
+//   let expected = 'my/path.jpg';
+//   assert.deepEqual(actual, expected);
+//
+//   // do not forget to leave the environment and DOM clean
+//   $('img[src="my/path.jpg"]').parent().remove();
+//   assert.end();
+// });
+//
+// test('INTEGRATION: clicking on button should display a profile of spider man', (assert) => {
+//   let path = 'my/path';
+//   let ext = 'jpg';
+//   let name = 'Spider-Man';
+//   let desc = 'Bitten by a spider';
+//   let stub = sinon.stub(handler, 'callMarvel');
+//
+//   render.renderFantasticButton('click me!');
+//   $('button').click();
+//   stub.returns(render.renderImage(path, ext, name, desc));
+//   let actual = $('img').css('display');
+//   let expected = 'block';
+//
+//   assert.deepEqual(actual, expected);
+//
+//   $('button').remove();
+//   $('img').parent().remove();
+//   stub.restore();
+//   assert.end();
+// });
+//
+// // remove
+// test('for presentation purposes', (assert) => {
+//   render.renderFantasticButton('click me!');
+//   assert.ok(true);
+//   assert.end();
+// });
